e671723dacf9b91727e9ecd3f21f804986007dda
Anna Zeng <annazeng@stanford.edu>
2017-05-07 20:48:46 -0700

    added clock_gettime test

--- test/time.c
+++ test/time.c
@@ -17,5 +17,10 @@ int main(int argc, const char *argv[])
     result = gettimeofday(&tv,NULL);
     assert(result == 0);
     printf("%lx,%lx\n", tv.tv_sec, tv.tv_usec);
+
+    struct timespec ts;
+    result = clock_gettime(CLOCK_REALTIME_FAST, &ts);
+    assert(result == 0);
+    printf("%lx,%lx\n", ts.tv_sec, ts.tv_nsec);
 }
 
