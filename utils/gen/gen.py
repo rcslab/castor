597e0af8e0d6f4d16e241686f5663646a66b683f
Tal Garfinkel <talg@cs.stanford.edu>
2017-08-31 18:30:17 -0700

    first cut at stand alone handler generator, still in progress.

---
+++ utils/gen/gen.py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python
+#XXX TODO
+#XXX -- finish handler generator
+#XXX -- generate header file
+#XXX -- move in a bunch of examples from events.c to make sure we support them.
+#XXX -- incorporate into runtime.
+#XXX -- migrate all handlers that can exploit this.
+#XXX -- document
+
+import sys
+
+line_number = 0
+
+cout = open("event_gen.c", "w")
+hout = open("event_gen.h", "w")
+
+def c_output(line):
+    print "cout", line
+    line = line + '\n'
+    cout.write(line)
+
+def h_output(line):
+    line = line + '\n'
+    hout.write(line)
+
+def die(msg):
+    print "Error on line %d:%s" % (line_number, msg)
+    sys.exit(1)
+
+def parse_dataspec(proto_spec, line):
+    data_spec = {}
+    _,specstr= line.split('$')
+    print "specstr:", specstr
+    specstr = specstr.strip()
+    if (specstr[0] != '{'):
+        die("missing opening brace")
+    if (specstr[-1] != '}'):
+        die("missing closing brace")
+    specstr = specstr.strip('{}')
+    parts = specstr.split('|')
+    if (len(parts) != 2):
+        die("missing portion of data spec, should be {result_str | arg_str}")
+    result_str, arg_str = parts
+
+    result_str = result_str.strip()
+    valid_results = ['result>0', 'result==0']
+    if not (result_str in valid_results):
+        die("\'%s\' not in %s" % (result_str, str(valid_results)))
+    else:
+        data_spec['result_cmp'] = result_str
+
+    arg_str = arg_str.strip()
+    arg_ar = arg_str.split(',')
+    log_args = {}
+    for arg_spec in arg_ar:
+        if len(arg_spec.split(':')) != 2:
+            die("invalid arg spec, should be arg_name:arg_type")
+        a_name, a_type = arg_spec.split(':')
+        a_name = a_name.strip()
+        a_type = a_type.strip()
+        #XXX: check arg name against proto spec
+        valid_types = ['sizeof']
+        if not (a_type in valid_types):
+            die("invalid argument type \'" + a_type + "\' not in " + str(valid_types))
+        log_args[a_name] = a_type
+    data_spec['log_args'] = log_args
+    return data_spec
+
+def parse_proto(line):
+    proto_spec = {}
+    if '$' in line:
+        line,_ = line.split('$')
+    if not (';') in line:
+        die('protocol line missing semi-colon')
+    else:
+        line,_ = line.split(';')
+    half = line.index('(')
+    prefix = line[:half]
+    print "prefix:" + prefix
+    result_type, name = prefix.split()
+    print "result_type:" + result_type
+    result_type = result_type.strip()
+    result_types = ['int', 'ssize_t']
+    if not (result_type in result_types):
+        die("unknown result type \'" + result_type + "\' not in " + str(result_types))
+    print "name:" + name
+    proto_spec['name'] = name
+    proto_spec['result_type'] = result_type
+
+    suffix = line[half:]
+    print "suffix:" + suffix
+    arg_string = suffix.strip('()')
+    print "arg_string:" + arg_string
+    proto_spec['arg_string'] = arg_string
+    args = arg_string.split(',')
+    print "args: ", args
+    argnames = map(lambda x: x.split()[-1].strip('*'), args)
+    print "arg names:", argnames
+    proto_spec['arg_names'] = argnames
+    argtypes = map(lambda x: x.split()[:-1], args)
+    print "arg types:", argtypes
+    proto_spec['arg_types'] = argtypes
+    return proto_spec
+
+#XXX: fix formatting
+def gen_handler(proto_spec, data_spec):
+    result_type = proto_spec['result_type']
+    name = proto_spec['name']
+    arg_string = proto_spec['arg_string']
+    arg_names = proto_spec['arg_names']
+    arg_types = proto_spec['arg_types']
+    leading_object = True if arg_types[0][0] == 'int' else False
+    object_string = 'O' if leading_object else ''
+    result_type_string = {'int' : 'I', 'ssize_t' : 'S'}[result_type]
+    record_method = "RRRecord%s%s" % (object_string, result_type_string)
+    replay_method = "RRReplay%s%s" % (object_string, result_type_string)
+    rr_event = "RREVENT_%s" % name.upper()
+
+    c_output("\n%s" % result_type)
+    c_output("__rr_%s(%s)" % (name, arg_string))
+    c_output("{")
+    c_output("\t%s result;\n" % result_type)
+    c_output("    switch (rrMode) {")
+
+    c_output("\tcase RRMODE_NORMAL:")
+    call_args = ["SYS_" + name] + arg_names
+    call_str =  "syscall(%s)" % ", ".join(call_args)
+    c_output("\t    return %s;"  % call_str)
+
+    c_output("\tcase RRMODE_RECORD:")
+    c_output("\t    result = %s;" % call_str)
+    if leading_object:
+        c_output("\t    %s(%s, %s, result);" % (record_method, rr_event, arg_names[0]))
+    else:
+        c_output("\t    %s(%s, result);" % (record_method, rr_event))
+
+    #XXX this is redudant between record/replay path.
+    if data_spec != None:
+        result_cmp_str = {'result==0': 'result == 0', 'result>0': 'result > 0'}[data_spec['result_cmp']]
+        c_output("    if (%s) {" % result_cmp_str)
+        #XXX: add support for using other arguments for data size
+        for name in data_spec['log_args'].keys():
+            c_output("\t\tlogData((uint8_t *)%s, sizeof(*%s));" % (name, name))
+        c_output("    }")
+    c_output("\t    break;")
+
+    c_output("\tcase RRMODE_REPLAY:")
+    if leading_object:
+        c_output("\t    %s(%s, %s, &result);" % (replay_method, rr_event, arg_names[0]))
+    else:
+        c_output("\t    %s(%s, &result);" % (replay_method, rr_event))
+    if data_spec != None:
+        result_cmp_str = {'result==0': 'result == 0', 'result>0': 'result > 0'}[data_spec['result_cmp']]
+        c_output("    if (%s) {" % result_cmp_str)
+        #XXX: add support for using other arguments for data size
+        for name in data_spec['log_args'].keys():
+            c_output("\t\tlogData((uint8_t *)%s, sizeof(*%s));" % (name, name))
+        c_output("    }")
+    c_output("\t    break;")
+    c_output("    }")
+    c_output("    return result;")
+    c_output("}")
+
+
+def parse_c_comment(line):
+    print "c_comment", line
+    c_output(line)
+
+def parse_preprocessor(line):
+    print "preprocessor", line
+    c_output(line)
+
+def parse_spec_comment(line):
+    print "spec_comment", line
+    return
+
+def parse_newline(line):
+    print "<newline>"
+    c_output('')
+
+for line in sys.stdin:
+    print line_number, line
+    line_number += 1
+    line = line.strip()
+
+    if line == '':
+        parse_newline(line)
+    elif line.startswith("#"):
+        parse_preprocessor(line)
+    elif line.startswith("//"):
+        parse_c_comment(line)
+    elif line.startswith("%"):
+        parse_spec_comment(line)
+    else:
+        try:
+            data_spec = None
+            proto_spec = parse_proto(line)
+            if "$" in line:
+                data_spec = parse_dataspec(proto_spec, line)
+            print "proto_spec", str(proto_spec)
+            print "data_spec", str(data_spec)
+            gen_handler(proto_spec, data_spec)
+        except:
+            print "Unknown parsing error on line %d" % line_number
+            raise
+
+cout.close()
+hout.close()
