3e64a819861fc7bf7c97c1b17249e65e96e05155
Tal Garfinkel <talg@cs.stanford.edu>
2018-03-14 19:15:07 -0700

    fixed truss output file names.

--- integration-tests/tests/pbzip2/test-record.sh
+++ integration-tests/tests/pbzip2/test-record.sh
@@ -2,5 +2,5 @@
 mkdir -p logs
 rm -rf logs/*
 
-record -o logs/zip.rr truss -o logs/truss.out sh -c "./pbzip2 -c -l -v -p2 words > words.bz2"
-record -o logs/unzip.rr truss -o logs/truss.out sh -c "./pbzip2 -c -d -l -v -p2 words.bz2 > words.out"
+record -o logs/zip.rr truss -o logs/zip.truss sh -c "./pbzip2 -c -l -v -p2 words > words.bz2"
+record -o logs/unzip.rr truss -o logs/unzip.truss sh -c "./pbzip2 -c -d -l -v -p2 words.bz2 > words.out"
--- integration-tests/tests/pbzip2/test-replay.sh
+++ integration-tests/tests/pbzip2/test-replay.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-replay -o logs/zip.rr truss -o logs/truss-r.out sh -c "./pbzip2 -c -l -v -p2 words > words.bz2"
-replay -o logs/unzip.rr truss -o logs/truss-r.out sh -c "./pbzip2 -c -d -l -v -p2 words.bz2 > words.out"
+replay -o logs/zip.rr truss -o logs/replay-zip.truss sh -c "./pbzip2 -c -l -v -p2 words > words.bz2"
+replay -o logs/unzip.rr truss -o logs/replay-unzip.truss sh -c "./pbzip2 -c -d -l -v -p2 words.bz2 > words.out"
