58d34ee9258614c997b2d8f8647ef820c69e0254
Tal Garfinkel <talg@cs.stanford.edu>
2018-06-16 01:26:15 +0000

    fix test code.

--- utils/gen/pretty_printer/test.c
+++ utils/gen/pretty_printer/test.c
@@ -2,16 +2,7 @@
 #include <errno.h>
 #include "castor_xlat.h"
 
-
-void
-pretty_print_getresuid_test(RRLogEntry entry)
-{
-    printf("getresuid() = %d\n", (int)entry.value[0]);
-}
-
-
 int main() {
     printf("EAGAIN = %s\n", xlat_errno(EAGAIN));
-
 }
 
