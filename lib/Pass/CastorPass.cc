3f8e1ab8a6178d371ec2370546ff9ddabfe80a1d
Haoyu Gu <guhaoyu2005@gmail.com>
2023-09-11 14:25:31 -0400

    Updated LLVM pass to work with the new pass manager
    Summary: Updated LLVM pass to work with the new pass manager
    
    Test Plan: test
    
    Reviewers: ali
    
    Reviewed By: ali
    
    Differential Revision: https://review.rcs.uwaterloo.ca/D831

--- lib/Pass/CastorPass.cc
+++ lib/Pass/CastorPass.cc
@@ -28,10 +28,21 @@
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
 #include "llvm/Transforms/Utils/ModuleUtils.h"
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
 
 #include "CastorPass.h"
 
-bool Castor::runOnModule(Module &M)
+namespace {
+
+bool doBasicBlock(Module &M, BasicBlock &BB);
+bool doAtomicRMW(Module &M, AtomicRMWInst *I);
+bool doAtomicCmpXchg(Module &M, AtomicCmpXchgInst *I);
+bool doLoad(Module &M, LoadInst *I);
+bool doStore(Module &M, StoreInst *I);
+bool doCall(Module &M, CallInst *I, int *rem);
+
+bool runPass(Module &M) 
 {
     bool mod = false;
 
@@ -45,12 +56,15 @@ bool Castor::runOnModule(Module &M)
 }
 
 // Basic Block Routines
-
-bool Castor::doBasicBlock(Module &M, BasicBlock &BB)
+bool doBasicBlock(Module &M, BasicBlock &BB)
 {
     bool mod = false;
+    int rem = 0;
+    std::vector<Instruction *> tbr;
+    Instruction *ri;
 
     for (auto &Inst : BB) {
+	rem = 0;
 	// Handle Atomics and Volatile load/store
 	AtomicRMWInst *ARMW = dyn_cast<AtomicRMWInst>(&Inst);
 	if (ARMW) {
@@ -75,14 +89,23 @@ bool Castor::doBasicBlock(Module &M, BasicBlock &BB)
 	// Handle RDTSC, RDRAND, inline asm, etc.
 	CallInst *CI = dyn_cast<CallInst>(&Inst);
 	if (CI) {
-	    mod |= doCall(M, CI);
+	    mod |= doCall(M, CI, &rem);
+	    if (rem) {
+		tbr.push_back(CI);
+	    }
 	}
+    }
+
+    while (tbr.size() > 0) {
+	ri = (Instruction *)(tbr.back());
+	tbr.pop_back();
+	ri->eraseFromParent();
     }
 
     return mod;
 }
 
-bool Castor::doAtomicRMW(Module &M, AtomicRMWInst *I)
+bool doAtomicRMW(Module &M, AtomicRMWInst *I)
 {
     errs() << "ATOMIC RMW\n";
 
@@ -96,10 +119,10 @@ bool Castor::doAtomicRMW(Module &M, AtomicRMWInst *I)
 
     FunctionCallee load_begin = M.getOrInsertFunction("__castor_rmw_begin",
 				Type::getVoidTy(ctx),
-				Type::getInt64Ty(ctx), NULL);
+				Type::getInt64Ty(ctx));
     FunctionCallee load_end = M.getOrInsertFunction("__castor_rmw_end",
 				Type::getVoidTy(ctx),
-				Type::getInt64Ty(ctx), NULL);
+				Type::getInt64Ty(ctx));
     Value *tmpAddr = before.CreatePtrToInt(addr, Type::getInt64Ty(ctx));
 
     before.CreateCall(load_begin, {tmpAddr});
@@ -108,7 +131,7 @@ bool Castor::doAtomicRMW(Module &M, AtomicRMWInst *I)
     return true;
 }
 
-bool Castor::doAtomicCmpXchg(Module &M, AtomicCmpXchgInst *I)
+bool doAtomicCmpXchg(Module &M, AtomicCmpXchgInst *I)
 {
     errs() << "ATOMIC CMP XCHG\n";
 
@@ -122,10 +145,10 @@ bool Castor::doAtomicCmpXchg(Module &M, AtomicCmpXchgInst *I)
 
     FunctionCallee load_begin = M.getOrInsertFunction("__castor_cmpxchg_begin",
 				Type::getVoidTy(ctx),
-				Type::getInt64Ty(ctx), NULL);
+				Type::getInt64Ty(ctx));
     FunctionCallee load_end = M.getOrInsertFunction("__castor_cmpxchg_end",
 				Type::getVoidTy(ctx),
-				Type::getInt64Ty(ctx), NULL);
+				Type::getInt64Ty(ctx));
     Value *tmpAddr = before.CreatePtrToInt(addr, Type::getInt64Ty(ctx));
 
     before.CreateCall(load_begin, {tmpAddr});
@@ -134,7 +157,7 @@ bool Castor::doAtomicCmpXchg(Module &M, AtomicCmpXchgInst *I)
     return true;
 }
 
-bool Castor::doLoad(Module &M, LoadInst *I)
+bool doLoad(Module &M, LoadInst *I)
 {
     if (I->getOrdering() != AtomicOrdering::NotAtomic || I->isVolatile()) {
 	errs() << "ATOMIC LOAD\n";
@@ -148,10 +171,10 @@ bool Castor::doLoad(Module &M, LoadInst *I)
 
 	FunctionCallee load_begin = M.getOrInsertFunction("__castor_load_begin",
 				Type::getVoidTy(ctx),
-				Type::getInt64Ty(ctx), NULL);
+				Type::getInt64Ty(ctx));
 	FunctionCallee load_end = M.getOrInsertFunction("__castor_load_end",
 				Type::getVoidTy(ctx),
-				Type::getInt64Ty(ctx), NULL);
+				Type::getInt64Ty(ctx));
 	Value *tmpAddr = before.CreatePtrToInt(addr, Type::getInt64Ty(ctx));
 
 	before.CreateCall(load_begin, {tmpAddr});
@@ -163,7 +186,7 @@ bool Castor::doLoad(Module &M, LoadInst *I)
     return false;
 }
 
-bool Castor::doStore(Module &M, StoreInst *I)
+bool doStore(Module &M, StoreInst *I)
 {
     if (I->getOrdering() != AtomicOrdering::NotAtomic || I->isVolatile()) {
 	errs() << "ATOMIC STORE\n";
@@ -177,10 +200,10 @@ bool Castor::doStore(Module &M, StoreInst *I)
 
 	FunctionCallee store_begin = M.getOrInsertFunction("__castor_store_begin",
 				Type::getVoidTy(ctx),
-				Type::getInt64Ty(ctx), NULL);
+				Type::getInt64Ty(ctx));
 	FunctionCallee store_end = M.getOrInsertFunction("__castor_store_end",
 				Type::getVoidTy(ctx),
-				Type::getInt64Ty(ctx), NULL);
+				Type::getInt64Ty(ctx));
 	Value *tmpAddr = before.CreatePtrToInt(addr, Type::getInt64Ty(ctx));
 
 	before.CreateCall(store_begin, {tmpAddr});
@@ -192,8 +215,10 @@ bool Castor::doStore(Module &M, StoreInst *I)
     return false;
 }
 
-bool Castor::doCall(Module &M, CallInst *I)
+bool doCall(Module &M, CallInst *I, int *rem)
 {
+    *rem = 0;
+
     if (I->isInlineAsm()) {
 	bool needRR = false;
 	errs() << "Inline Assembly\n";
@@ -219,9 +244,9 @@ bool Castor::doCall(Module &M, CallInst *I)
 	    IRBuilder<> after(ni);
 
 	    FunctionCallee asm_begin = M.getOrInsertFunction("__castor_asm_begin",
-				Type::getVoidTy(ctx), NULL);
+				Type::getVoidTy(ctx));
 	    FunctionCallee asm_end = M.getOrInsertFunction("__castor_asm_end",
-				Type::getVoidTy(ctx), NULL);
+				Type::getVoidTy(ctx));
 
 	    before.CreateCall(asm_begin);
 	    after.CreateCall(asm_end);
@@ -244,8 +269,7 @@ bool Castor::doCall(Module &M, CallInst *I)
 	IRBuilder<> after(ni);
 
 	FunctionCallee castor_rdtsc = M.getOrInsertFunction("__castor_rdtsc",
-				Type::getInt64Ty(ctx),
-				NULL);
+				Type::getInt64Ty(ctx));
 	Value *rdtsc = after.CreateCall(castor_rdtsc);
 
 	for (auto &U : I->uses()) {
@@ -253,6 +277,7 @@ bool Castor::doCall(Module &M, CallInst *I)
 	    user->setOperand(U.getOperandNo(), rdtsc);
 	}
 
+	*rem = 1;
 	return true;
     }
 
@@ -261,6 +286,44 @@ bool Castor::doCall(Module &M, CallInst *I)
     return false;
 }
 
+
+bool Castor::runOnModule(Module &M)
+{
+    return runPass(M);
+}
+
+struct CastorPass : public PassInfoMixin<CastorPass> {
+  PreservedAnalyses run(Module &M, ModuleAnalysisManager &) {
+	if (runPass(M)) {
+		return PreservedAnalyses::none();
+	} else {
+  		return PreservedAnalyses::all();
+	}
+  }
+};
+
+} //namespace
+
+/* New PM Registration */
+llvm::PassPluginLibraryInfo getCastorPluginInfo() {
+  return {LLVM_PLUGIN_API_VERSION, "castor", LLVM_VERSION_STRING,
+          [](PassBuilder &PB) {
+            PB.registerOptimizerLastEPCallback(
+                [](llvm::ModulePassManager &MPM, OptimizationLevel ol) {
+                   	MPM.addPass(CastorPass());
+                    	return true;
+                });
+          }};
+}
+
+#ifndef LLVM_BYE_LINK_INTO_TOOLS
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+  return getCastorPluginInfo();
+}
+#endif
+
+/* Legacy PM Registration */
 char Castor::ID = 0;
 static RegisterPass<Castor> CP("castor", "Castor Record/Replay Pass");
 
@@ -271,4 +334,5 @@ static void loadPass(const PassManagerBuilder &Builder, legacy::PassManagerBase 
 
 static RegisterStandardPasses clangtoolLoader_Ox(PassManagerBuilder::EP_OptimizerLast, loadPass);
 static RegisterStandardPasses clangtoolLoader_O0(PassManagerBuilder::EP_EnabledOnOptLevel0, loadPass);
+
 
