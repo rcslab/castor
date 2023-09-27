68dbf671b729a26038283b41377dccce42b6e3de
Haoyu Gu <guhaoyu2005@gmail.com>
2023-09-27 17:28:18 -0400

    Update sysroot.sh
    Summary: Update sysroot.sh
    
    Test Plan: test
    
    Reviewers: ali, yahya
    
    Reviewed By: ali
    
    Differential Revision: https://review.rcs.uwaterloo.ca/D858

--- utils/sysroot.sh
+++ utils/sysroot.sh
@@ -2,7 +2,10 @@
 
 set BASE=`pwd`
 
-if ( ! -d /usr/src ) then
+echo "Name for Castor jail?"
+set JAIL_NAME = $<
+
+if ( ! -d $BASE/sysroot-src ) then
     echo "Requires FreeBSD source code to be installed."
     exit 1
 endif 
@@ -10,22 +13,63 @@ endif
 set NCPU=`/sbin/sysctl -n hw.ncpu`
 set NCPU=`expr 2 \* $NCPU`
 
-if ( ! -d $BASE/sysroot-src ) then
-    cp -r /usr/src $BASE/sysroot-src
+cd $BASE/sysroot-src/
 
-    cd $BASE/sysroot-src/lib
-    patch -p1 < $BASE/utils/patches/libthr.diff
+echo "Patching FreeBSD source.."
+if ( -f "lib/libc/sys/fstatat.c" ) then
+    patch -u -b lib/libc/sys/fstatat.c -i ../utils/patches/libc/fstatat.c.diff
+else
+    echo "lib/libc/sys/fstatat.c does not exist. Aborted."
+    exit 1
 endif
 
+if ( -f "lib/libc/sys/getdents.c" ) then
+    patch -u -b lib/libc/sys/getdents.c -i ../utils/patches/libc/getdents.c.diff
+else
+    echo "lib/libc/sys/getdents.c does not exist. Aborted."
+    exit 1
+endif
+
+if ( -f "lib/libc/sys/lstat.c" ) then
+    patch -u -b lib/libc/sys/lstat.c -i ../utils/patches/libc/lstat.c.diff
+else
+    echo "lib/libc/sys/lstat.c does not exist. Aborted."
+    exit 1
+endif
+
+if ( -f "lib/libc/sys/stat.c" ) then
+    patch -u -b lib/libc/sys/stat.c -i ../utils/patches/libc/stat.c.diff
+else
+    echo "lib/libc/sys/stat.c does not exist. Aborted."
+    exit 1
+endif
+
+if ( -f "lib/libthr/pthread.map" ) then
+    patch -u -b lib/libthr/pthread.map -i ../utils/patches/libthr/pthread.map.diff
+else
+    echo "lib/libthr/pthread.map does not exist. Aborted."
+    exit 1
+endif
+
+if ( -f "lib/libthr/thread/thr_mutex.c" ) then
+    patch -u -b lib/libthr/thread/thr_mutex.c -i ../utils/patches/libthr/thr_mutex.c.diff
+else
+    echo "lib/libthr/thread/thr_mutex.c does not exist. Aborted."
+    exit 1
+endif
+
+
+echo "Building FreeBSD source.."
 cd $BASE/sysroot-src
-make -j $NCPU xdev XDEV=amd64 XDEV_ARCH=amd64 MAKEOBJDIRPREFIX=$BASE/sysroot-obj DESTDIR=$BASE/sysroot
-cd ..
+make -j $NCPU buildworld MAKEOBJDIRPREFIX=$BASE/sysroot-obj NO_FSCHG=YES
+make installworld DESTDIR=$BASE/sysroot 
 
 #include Castor runtime at loadtime
+echo "Patching Castor jail.."
 cd $BASE/sysroot
-sed -i .backup 's;(;( /usr/lib/libCastorRuntime.a;' usr/amd64-freebsd/usr/lib/libc.so
-rm usr/amd64-freebsd/usr/lib/libthr.so
-echo "GROUP ( /usr/lib/libCastorThreadRuntime.a /lib/libthr.so.3)" >> usr/amd64-freebsd/usr/lib/libthr.so
+sed -i .backup 's;(;( /usr/lib/libCastorRuntime.a;' usr/lib/libc.so
+rm usr/lib/libthr.so
+echo "GROUP ( /usr/lib/libCastorThreadRuntime.a /lib/libthr.so.3)" >> usr/lib/libthr.so
 
 echo "SYSROOT Build Complete"
 
