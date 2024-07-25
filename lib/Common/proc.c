2df11612cc8fc6c54bfc28ee0eb9aef99628cd46
hao <guhaoyu2005@gmail.com>
2024-07-25 15:15:43 +0000

    Remove the last bit of libsys (LD_PRELOAD and LD_BITMAP) from Castor Spawn
    Summary: Remove the last bit of libsys (LD_PRELOAD and LD_BITMAP) from Castor Spawn
    
    Test Plan: test
    
    Reviewers: ali
    
    Reviewed By: ali
    
    Differential Revision: https://review.rcs.uwaterloo.ca/D892

--- lib/Common/proc.c
+++ lib/Common/proc.c
@@ -70,19 +70,6 @@ Spawn(bool pinned, int maxcpus, char *const argv[])
 	PinProcess(maxcpus);
     }
 
-    // Setup LD_LIBMAP
-    char *cdir = getenv("CASTORDIR");
-    if (cdir == NULL) {
-	Debug_Log(LEVEL_SYS, "Please set CASTORDIR to point to the directory containing libsys_castor.so\n");
-	abort();
-    }
-    char libsyspath[512];
-    char ldpreload[512];
-    snprintf(libsyspath, 512, "libsys.so.7=%s/libsys_castor.so,libsys_castor.so=%s/libsys_castor.so", cdir, cdir);
-    snprintf(ldpreload, 512, "%s/../ThreadRuntime/libcastor_thr.so", cdir);
-    setenv("LD_LIBMAP", libsyspath, 0);
-    setenv("LD_PRELOAD", ldpreload, 0);
-
     execvp(*argv, argv);
 
     // PERROR("execv");
